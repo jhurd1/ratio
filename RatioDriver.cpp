7690afa90dd2dde757a042944c6b8dd6d0048c0f
jhurd1 <jamiehurd3@gmail.com>
2022-07-28 15:56:40 -0600

    std::cin doesn't like nullptr

--- RatioDriver.cpp
+++ RatioDriver.cpp
@@ -1,5 +1,6 @@
 #include "ratio.h"
 #include <iostream>
+#include <vector>
 
 /*************************************
  * Driver function for Ratio.
@@ -7,13 +8,21 @@
 int main(int argc, char *argv[])
 {
     Ratio r;
-    int *i = nullptr;
-    std::vector<int> *tester = nullptr;
-    std::cout << "Provide integers for the vector" << std::endl;
-    while(std::cin >> *i)
+    //int *i = nullptr;
+    int i = 0;
+    std::vector<int> *tester = new std::vector<int>;
+    try
     {
-     tester->push_back(*i);
+     std::cout << "Provide integers for the vector" << std::endl;
+     while(std::cin >> i) //bad access here
+     {
+      tester->push_back(i);
+     }
+     r.showRatio(i, tester);
+    } catch(std::exception &e)
+    {
+     std::cout << "Something went wrong with the user input and function call block." << std::endl;
     }
-    r.showRatio(*i, tester);
+    delete tester;
     return 0;
 }
