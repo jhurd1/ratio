d42304b9d62c7e416b8b4cc81b1753f80f01f506
jhurd1 <jamiehurd3@gmail.com>
2022-08-12 11:35:42 -0600

    iterator no dice on right count either

--- ratio.cpp
+++ ratio.cpp
@@ -59,10 +59,12 @@ std::vector<int> Ratio::gettester() const
 int Ratio::showRatio(std::vector<int> *tester)
 {
   float count = 0;
+  float itCount = 0;
   int loopCycles = 0;
   for(unsigned int i = 1; i < tester->size(); i++)
   {
    loopCycles++;
+   std::cout << "Unsigned int i is " << i << "\n" << std::endl;
    std::cout << "Loop has cycled: " << loopCycles << " " << " times!" << std::endl;
    if(tester[i].size() == tester[i - 1].size())
    {
@@ -72,7 +74,12 @@ int Ratio::showRatio(std::vector<int> *tester)
   for(std::vector<int>::const_iterator j = tester->begin(); j != tester->end(); ++j)
   {
    std::cout << *j << " ";
+   if(j == j - 1)
+   {
+    itCount++;
+   }
   }
+  std::cout << "\n" << "The iterator calculation is " << std::fixed << std::setprecision(6) << itCount / tester->size() << std::endl;
   std::cout << "\n" << std::fixed << std::setprecision(6) << count / tester->size() << std::endl;
   return 0;
  }
