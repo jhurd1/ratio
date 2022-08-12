ee9eff928788341a86129ea5e44be4f32443bb80
jhurd1 <jamiehurd3@gmail.com>
2022-08-12 11:47:35 -0600

    ratio.cpp

--- ratio.cpp
+++ ratio.cpp
@@ -64,22 +64,24 @@ int Ratio::showRatio(std::vector<int> *tester)
   for(unsigned int i = 1; i < tester->size(); i++)
   {
    loopCycles++;
-   std::cout << "Unsigned int i is " << i << "\n" << std::endl;
-   std::cout << "Loop has cycled: " << loopCycles << " " << " times!" << std::endl;
+   std::cout << "The index for i is " << i << "\n" << std::endl;
+   std::cout << "Loop has cycled: " << loopCycles << " " << " times!" << "\n" << std::endl;
    if(tester[i].size() == tester[i - 1].size())
    {
     count++;
    }
-  }
   for(std::vector<int>::const_iterator j = tester->begin(); j != tester->end(); ++j)
   {
-   std::cout << *j << " ";
+   std::cout << "The value at index " << count << " is " << *j << " ";
    if(j == j - 1)
    {
     itCount++;
    }
   }
+  }
+  
+  std::cout << "\n";
   std::cout << "\n" << "The iterator calculation is " << std::fixed << std::setprecision(6) << itCount / tester->size() << std::endl;
-  std::cout << "\n" << std::fixed << std::setprecision(6) << count / tester->size() << std::endl;
+  std::cout << "\n" << "The unsigned int loop calculation constitutes " << std::fixed << std::setprecision(6) << count / tester->size() << "\n" << std::endl;
   return 0;
  }
